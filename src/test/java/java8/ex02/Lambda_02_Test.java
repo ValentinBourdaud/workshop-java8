669e0693e1167fb1ea0b1073f40a005fc0e29721
Valentin Bourdaud <bourdaudv@gmail.com>
2018-04-17 16:46:40 +0200

    Fin exo 2 lambda - Test passant

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -24,8 +24,11 @@ public class Lambda_02_Test {
 
     // tag::map[]
     private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
-        // TODO implémenter la méthode
-        return null;
+    	List<Account> mappedList = new ArrayList<Account>();
+    	for(int i = 0; i<personList.size();i++)
+    		mappedList.add(mapper.map(personList.get(i)));
+		return mappedList; 
+    	
     }
     // end::map[]
 
@@ -35,10 +38,18 @@ public class Lambda_02_Test {
     public void test_map_person_to_account() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
+        
+        PersonToAccountMapper pa= p->{
+        	Account account = new Account();
+        	account.setBalance(100);
+        	account.setOwner(p);
+        	return account;
+        };
+    
 
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        List<Account> result = map(personList, pa);
 
         assertThat(result, hasSize(personList.size()));
         assertThat(result, everyItem(hasProperty("balance", is(100))));
@@ -51,13 +62,18 @@ public class Lambda_02_Test {
     public void test_map_person_to_firstname() throws Exception {
 
         List<Person> personList = Data.buildPersonList(100);
-
-        // TODO transformer la liste de personnes en liste de prénoms
-        List<String> result = null;
+        List<String> stringList = new ArrayList<>();
+        
+       for (Person person : personList)
+       
+    	   stringList.add(person.getFirstname());
+       
+       List<String> result = stringList;
 
         assertThat(result, hasSize(personList.size()));
         assertThat(result, everyItem(instanceOf(String.class)));
         assertThat(result, everyItem(startsWith("first")));
-    }
+    };
     // end::test_map_person_to_firstname[]
 }
+
